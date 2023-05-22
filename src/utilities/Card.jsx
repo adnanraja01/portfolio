7f8b886e24b63752e85571b48cc0dba3104624bf
adnanraja01 <adnanaltafraja@gmail.com>
2023-05-22 19:00:36 +0530

    first commit

---
+++ src/utilities/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import svg from "../assets/svg/github.svg";
+
+const Card = (props) => {
+  return (
+    <div className=" z-30 border rounded-2xl border-white/10 px-10 py-[3rem] h-full bg-[#0F0F11]">
+      <img src={props.src} alt="icon" />
+
+      <h2 className="text-white text-s18 lg:text-s20 font-bold mt-8">
+        {props.title}
+      </h2>
+      <p className="text-grey text-s16 lg:text-s18 mt-[1rem]">{props.info}</p>
+    </div>
+  );
+};
+
+export default Card;
+// Clean, modern and responsive designs - optimized for performance, search
+// engines, and converting users to customers.
